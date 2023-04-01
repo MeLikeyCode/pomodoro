bf137b78daa5f777700c97b7a4bfd6b501c6791f
MeLikeyCode <abdullah.aghazadah@gmail.com>
2023-04-01 19:54:38 -0400

    if application is already launched (and in the background), just untray it instead of launching a new instance

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from pomodoroapp import PomodoroApp
-from pomodoro import Pomodoro
-import time
+import psutil
+
+def process_exists(process_name):
+    """Returns true if a process with the specified name is running."""
+    for process in psutil.process_iter(['name']):
+        if process.info['name'] == process_name:
+            return True
+    return False
 
 if __name__ == "__main__":
+    # if app is already running, untray it and quit
+    if process_exists("pomodoro.exe"):
+        from win32gui import FindWindow, ShowWindow, SetForegroundWindow
+        from win32con import SW_RESTORE, SW_SHOW
+        hwnd = FindWindow(None, "Pomodoro")
+        ShowWindow(hwnd, SW_RESTORE)
+        SetForegroundWindow(hwnd)
+        exit(0)
+
     app = PomodoroApp()
     app.start()
     
