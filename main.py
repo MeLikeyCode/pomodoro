9928c73c8c32bc88b198dc0cc318afb51d3afb9f
MeLikeyCode <abdullah.aghazadah@gmail.com>
2023-04-02 21:39:47 -0400

    fix bug related to not launching multiple instances

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from pomodoroapp import PomodoroApp
-import psutil
-
-def process_exists(process_name):
-    """Returns true if a process with the specified name is running."""
-    for process in psutil.process_iter(['name']):
-        if process.info['name'] == process_name:
-            return True
-    return False
+import msvcrt
+import tempfile
+from win32gui import FindWindow, ShowWindow, SetForegroundWindow
+from win32con import SW_RESTORE
+import os
 
 if __name__ == "__main__":
-    # if app is already running, untray it and quit
-    if process_exists("pomodoro.exe"):
-        from win32gui import FindWindow, ShowWindow, SetForegroundWindow
-        from win32con import SW_RESTORE, SW_SHOW
-        hwnd = FindWindow(None, "Pomodoro")
+    # check if another instance of Pomodoro is already running
+    lock_file_path = tempfile.gettempdir() + "/pomodoro.lock"
+    lock_file = open(lock_file_path, 'w') # create the lock file if it doesn't exist
+    try:
+        # try locking the "lock" file
+        msvcrt.locking(lock_file.fileno(), msvcrt.LK_NBLCK, 1)
+    except IOError:
+        # failed to lock it, thus another instance locked it (and is thus running)
+        print("An instance of Pomodoro is already running")
+        # show the window of the running instance, and exit
+        hwnd = FindWindow(None, "pomodoro")
         ShowWindow(hwnd, SW_RESTORE)
         SetForegroundWindow(hwnd)
-        exit(0)
+        os._exit(0)
 
     app = PomodoroApp()
     app.start()
